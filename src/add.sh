7d2d8bb9f3bb7529d11bb6036e6943aefce37b29
psb-david-petty <david_petty@psbma.org>
2019-11-19 23:00:11 -0500

    Snapshot of student webpages.

--- src/add.sh
+++ src/add.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Command to git add files from files.txt. tr here is to allow "'" in filenames.
-CMD="cat files.txt |tr '\n' '\0' |xargs -0 git add"
+CMD="cat files.txt |tr '\n' '\0' |xargs -t -n1 -0 git add"
 
 # ${1} should be "-n" for dryrun.
 if [ "${#}" -gt 0 ]; then
-    echo "${CMD} ${1}"
+    printf "%s\n" "${CMD} ${1}"
     eval "${CMD} ${1}"
 else
-    echo "${CMD}"
+    printf "%s\n" "${CMD}"
     eval "${CMD}"
 fi
