f249f17b3969e345a33faf71f89b06edad17a76e
psb-david-petty <david_petty@psbma.org>
2020-01-12 10:31:09 -0500

    updated form js files.

--- www/form/form.js
+++ www/form/form.js
@@ -1,70 +1,59 @@
-function tagColor(tag, color) {
-    // Set the tag color attribute to color.
-    for (let node of document.getElementsByTagName(tag)) {
-        node.style.color = color;
-    }
-}
-
-// https://css-tricks.com/snippets/javascript/htmlentities-for-javascript/
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-
-function showFormValues() {
+/* Return HTML for form input values for all elements of all forms.
+ */
+function showFormInputValues() {
     const
         div = '|',
         tagValues = [ "select", "textarea", ],
-        // The only tags handled are these:
+        /* the only tags handled are these: */
         tags = [ "input", ].concat(tagValues),
-        // The only input types handled are these:
+        /* the only input types handled are these: */
         inputValues = [ "file", "hidden", "password", "text",
-            // Input types aded in HTML-5.
+            /* input types added in HTML-5: */
             "color", "email", "number", "range", "search", "tel", "url",
             "date", "datetime-local", "month", "week", "time",
         ],
-        // Input types with .checked attribute.
+        /* input types with .checked attribute: */
         checkedValues = [ "radio", "checkbox", ];
     let html = '',  name = '';
 
-    // Process input from all forms (form) and elements (node) in those forms.
-    for (let form of document.getElementsByTagName('form')) {
-        // console.log(new XMLSerializer().serializeToString(f))
+    /* process input from all forms (form) and elements (node) in those forms */
+    for (let form of document.querySelectorAll('form')) {
+        /* add <h3> header w/ form and its id */
         let text = (form.id)
             ? `<h3>Form: id="${form.id}"</h3><p>`
             : `<h3>Form:</h3><p>`;
         for (let node of form) {
-            // console.log(new XMLSerializer().serializeToString(e));
             let tagName = node.tagName.toLowerCase();
-            // Process input from all tags named in tags.
+            /* process input from all tags named in tags */
             if (tags.indexOf(tagName) >= 0) {
-                // Booleans for supported value tags. node.type is lowercase.
+                /* set Booleans for supported value tags; node.type is lowercase */
                 let supportedValueTag = tagValues.indexOf(tagName) >= 0,
                     supportedInputValueType = tagName == 'input'
                         && inputValues.indexOf(node.type) >= 0,
                     supportedInputCheckedType = tagName == 'input'
                         && checkedValues.indexOf(node.type) >= 0;
-                // Set divider around processed inputs... w/o common name...
+
+                /* set divider around processed inputs... w/o common name... */
                 if (node.name && node.name.toLowerCase() != name) {
                     name = node.name.toLowerCase();
                     text += `${div} ${name}: `;
                 }
-                // ...and w/o *any* name.
+                /* ...and w/o *any* name */
                 if (!node.name && (supportedValueTag 
                     || supportedInputValueType || supportedInputCheckedType)) {
                     name = '';
                     text += `${div} `;
                 }
-                // Form values for tags.
+
+                /* form values for tags */
                 if (supportedValueTag && node.value) {
                     text += `"${node.value}" `;
                 }
-                // Form values for input types.
+                /* form values for input types */
                 if (supportedInputValueType && node.value) {
                     text += `"${node.value}" `;
-                    if (node.type == 'color')   /* set forms to color */
-                        tagColor('form', node.value);
                 }
-                // Form values for checked inputs.
+                /* form values for checked input types */
                 if (supportedInputCheckedType) {
                     text += `[${node.checked}] `;
                 }
@@ -77,6 +66,8 @@ function showFormValues() {
     return html;
 }
 
+/* Return HTML for all form element attributes, including <pre> for code itself.
+ */
 function showFormElementAttributes() {
     let html = '';
     const tags = [
@@ -88,8 +79,8 @@ function showFormElementAttributes() {
     //for (let i = 0; i < tags.length; i++) {
     for (let tag of tags) {
         let text = '';
-        //for (let j = 0; (t = document.getElementsByTagName(tags[i])[j]); j++) {
-        for (let node of document.getElementsByTagName(tag)) {
+        //for (let j = 0; (t = document.querySelectorAll(tags[i])[j]); j++) {
+        for (let node of document.querySelectorAll(tag)) {
             //for (let k = 0; (a = t.attributes[k]); k++) {
             for (let attr of node.attributes) {
                 text += `${attr.name} = "${attr.value}" `;
@@ -116,16 +107,55 @@ function showFormElementAttributes() {
     return html;
 }
 
-/*
- * Show form input values in the element with id="inputs" and
- * form element attributes in the element with id="outputs".
+/* Create closure for event on setSelector to change color on changeSelector.
  */
-function show() {
-    document.getElementById("inputs").innerHTML = '';
-    document.getElementById("output").innerHTML = '';
-    var inputs = showFormValues();
+function setWatchColorPicker(setSelector, changeSelector) {
+    /* closure to change color on changeSelector in response to event */
+    function watchColorPicker(event) {
+        document.querySelectorAll(changeSelector).forEach(function(s) {
+            s.style.color = event.target.value;
+        });
+    }
+    node = document.querySelector(setSelector);
+    if (node) {
+        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color
+        node.addEventListener("input", watchColorPicker, false);
+        node.addEventListener("change", watchColorPicker, false);
+        // node.select();  /* select text content of the color input */
+        //                 /* if control is implemented as a text field */
+        console.log(`setWatchColorPicker: watching "${setSelector}" to change color of "${changeSelector}"`);
+    }
+}
+
+/* Show form values in the element with id formID and form
+ * element attributes in the element with id elementID.
+ */
+function show(formSelector, elementSelector) {
+    document.querySelector(formSelector).innerHTML = '';
+    document.querySelector(elementSelector).innerHTML = '';
+    var inputs = showFormInputValues();
     var outputs = showFormElementAttributes();
-    document.getElementById("inputs").innerHTML = inputs;
-    document.getElementById("output").innerHTML = outputs;
+    document.querySelector(formSelector).innerHTML = inputs;
+    document.querySelector(elementSelector).innerHTML = outputs;
     return false;
+}
+
+/* Replace HTML characters w/ their character entities.
+ * https://css-tricks.com/snippets/javascript/htmlentities-for-javascript/
+ */
+function htmlEntities(str) {
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/\|/g,'&vert;');
+}
+
+/* Set selector color to color.
+ */
+function selectorColor(selector, color) {
+    for (let node of document.querySelectorAll(selector)) {
+        node.style.color = color;
+    }
 }
--- www/form/template.js
+++ www/form/template.js
@@ -1,23 +1,21 @@
-/* show();
- *
- * Look at every element in every form and, if it is one of "input" (if
+/* Look at every element in every form and, if it is one of "input" (and if
  * element's type is *not* "checkbox" or "radio"), "select", or "textarea"
  * and if it has a name, set internalHTML of element whose id matches name 
  * to node's value.
  *
- * If element is "input" and element's type is "checkbox" or "radio", 
+ * If element is "input" and element's type *is* "checkbox" or "radio", 
  * - Create maps w/ keys for every token in "data-valid" attribute of form.
  * - For every checkbox whose "name" matches a key and every "radio" whose
  *   "class" contains a key:
  *   - count number of elements containing "value", storing them in a map
  *
  * Finally:
- * - call showMaps(maps);           // to set elements' internalHTML matching keys
- * - call hideShow(hideId, showId); // to reveal element with id showId
+ * - call showMaps(maps);                       // to set elements' internalHTML matching keys
+ * - call hideShow(hideSelector, showSelector); // to reveal element with selector showSelector
  */
-function show(hideId, showId) {
+function show(hideSelector, showSelector) {
     let maps = initializeCheckboxRadioMap('form', 'data-valid');
-    for (let form of document.getElementsByTagName('form')) {
+    for (let form of document.querySelectorAll('form')) {
         console.log(form);
         for (let node of form.elements) {
             console.log(node);
@@ -28,8 +26,10 @@ function show(hideId, showId) {
                 || [ "input", ].indexOf(tagName) >= 0
                     && [ "checkbox", "radio", ].indexOf(node.type) < 0)
                     && node.name) {
-                if (document.getElementById(node.name)) {
-                    document.getElementById(node.name).innerHTML = node.value;
+                let selector = `#${node.name}`;
+                if (document.querySelector(selector)) {
+                    document.querySelector(selector).innerHTML = node.value;
+                    logHTML(selector);
                 }
             }
 
@@ -61,22 +61,22 @@ function show(hideId, showId) {
         }
     }
     showMaps(maps);
-    hideShow(hideId, showId);
+    hideShow(hideSelector, showSelector);
     return false;
 }
 
 /* Return a Map of maps w/ keys from space-separated tokens in attr of tag 
- * w/ id and values initialized to new Maps.
+ * w/ selector and values initialized to new Maps.
  */
-function initializeCheckboxRadioMap(id, attr) {
+function initializeCheckboxRadioMap(selector, attr) {
     let maps = new Map(),
-        node = document.getElementById(id);
+        node = document.querySelector(selector);
     if (node && node.getAttribute(attr)) {
         for (let key of node.getAttribute(attr).split(' ')) {
             maps.set(key, new Map());
         }
     }
-    console.log(`map: ${Array.from(maps)};`);
+    console.log(Array.from(maps));
     return maps;
 }
 
@@ -87,15 +87,16 @@ function initializeCheckboxRadioMap(id, attr) {
 function showMaps(maps) {
     for (let mapsKey of maps.keys()) {
         let map = maps.get(mapsKey);
-        node = document.getElementById(mapsKey);
+        let selector = `#${mapsKey}`;
+        node = document.querySelector(selector);
         console.log(map, node);
         if (node) {
             for (let key of map.keys()) {
                 /* ADD CHECKBOX / RADIO MAP PROCESSING HERE */
                 node.innerHTML = `${mapsKey}=${Array.from(map)}`
                     + ' /* DO SOMETHING WITH THESE VALUES */';
-                console.log(`document.getElementById('${key}').InnerHTML = '${node.innerHTML}'`);
             }
+            logHTML(selector);
         }
     }
 }
@@ -103,9 +104,38 @@ function showMaps(maps) {
 /* Hide tag w/ id hideID by setting 'display: none;' and show tag w/
  * id showID by setting 'display: block;'.
  */
-function hideShow(hideID, showID) {
-    if (document.getElementById(hideID))
-        document.getElementById(hideID).style.display = 'none';
-    if (document.getElementById(showID))
-        document.getElementById(showID).style.display = 'block';
+function hideShow(hideSelector, showSelector) {
+    if (document.querySelector(hideSelector))
+        document.querySelector(hideSelector).style.display = 'none';
+    if (document.querySelector(showSelector))
+        document.querySelector(showSelector).style.display = 'block';
+}
+
+/* Log innerHTML for selector.
+ */
+function logHTML(selector) {
+    let node = document.querySelector(selector);
+    if (node) {
+        console.log(`querySelector('${selector}').innerHTML="${node.innerHTML}"`);
+    }
+}
+
+/* Replace HTML characters w/ their character entities.
+ * https://css-tricks.com/snippets/javascript/htmlentities-for-javascript/
+ */
+function htmlEntities(str) {
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/\|/g,'&vert;');
+}
+
+/* Set selector color to color.
+ */
+function selectorColor(selector, color) {
+    for (let node of document.querySelectorAll(selector)) {
+        node.style.color = color;
+    }
 }
